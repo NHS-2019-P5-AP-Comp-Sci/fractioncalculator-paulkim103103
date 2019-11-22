5fe83e7b8c2dc9515beb441ae26db79af86606d8
s-kimp <s-kimp@53S50030548.it.bsd405.org>
2019-11-22 13:39:27 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,26 +3,63 @@
  */
 
 package fracCalc;
-import java.util.*;
+import java.util.Scanner;
 public class FracCalc {
     public static void main(String[] args)
     {
-		Scanner frac = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Paul's Fraction Calculator");
-		System.out.print("Input: ");
-		String userResponse= frac.nextLine();
-		System.out.println(produceAnswer(userResponse));
+		System.out.print("What is your equation? ");
+		String response = s.nextLine();
+		while(!response.equalsIgnoreCase("quit")) {
+		System.out.println(produceAnswer(response));
+		System.out.print("What is your equation? ");
+		response = s.nextLine();
     	// TODO: Read the input from the user and call produceAnswer with an equation
-
     }
+    }
+		
     public static String produceAnswer(String input){
-    	Scanner frac = new Scanner(input);
-    	String fraction1 = frac.next();
-    	String operator = frac.next();
-    	String fraction2 = frac.next();
+    	String frac = input;
+    	String firstfrac = frac.substring(0, frac.indexOf(' '));
+    	frac = frac.substring(frac.indexOf(' ') + 1);
+    	String operator = frac.substring(0, frac.indexOf(' '));
+    	frac = frac.substring(frac.indexOf(' ') + 1);
+    	String secFrac = frac;
     	
-    	return fraction2;
+    	String Whole2 = findWhole(secFrac);
+    	String Num2 = findNum(secFrac);
+    	String Denom2 = findDenom(secFrac);
+
+    	String answer2 = "Whole:" + Whole2 + " numerator:" + Num2 + " denominator:" + Denom2;
+    	return answer2;
+    }
+    	
+    public static String findWhole(String str) {
+    	if(str.contains("_")) {
+    		return str.substring(0, str.indexOf('_'));
+    	}else if(str.contains("/")) {
+    		return "0";
+    	}else return str;
+    }
+    
+    public static String findNum(String str) {
+    	if(str.contains("_")) {
+    		return str.substring(str.indexOf('_') + 1, str.indexOf('/'));
+    	}else if(str.contains("/")) {
+    		return str.substring(0, str.indexOf('/'));
+    	}else {
+    		return "0";
+    	}
+    }
+    
+    public static String findDenom(String str) {
+    	if(str.contains("/")) {
+    		return str.substring(str.indexOf("/")+1);
+    	}else {
+    		 return "1";
+    		
+    	}
     }
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
